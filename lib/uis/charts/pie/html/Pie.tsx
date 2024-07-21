311d03d03b5a84107c6fcbeb2775d1544e12a55d
artiphishle <fabio.gartenmann@protonmail.com>
2024-07-21 18:43:33 +0200

    Add Pie Chart

---
+++ lib/uis/charts/pie/html/Pie.tsx
@@ -0,0 +1,9 @@
+import "./pie.css";
+
+export function Pie({ percentage }: IAnkhUiChartPie) {
+  return <div style={{ animationDelay: `-${percentage}s` }} className="pie">{`${percentage}%`}</div>
+}
+
+interface IAnkhUiChartPie {
+  percentage: number;
+}
