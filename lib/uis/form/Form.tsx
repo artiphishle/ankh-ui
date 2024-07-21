d7248925e30f0138a4f510cda81f36884295966b
artiphishle <fabio.gartenmann@protonmail.com>
2024-07-21 02:08:45 +0200

    Add Google Prettier config

--- lib/uis/form/Form.tsx
+++ lib/uis/form/Form.tsx
@@ -1,34 +1,26 @@
 "use client";
 import { ahooks } from 'ankh-hook';
-import {Button} from "@/uis/button/Button";
+import { Button } from "@/uis/button/Button";
 import "./form.css";
 import { Auth } from '@/auth/Auth';
 
-export function Form(){
+export function Form() {
   const { list, remove, batchRemove, getKey, insert, replace } = ahooks.useDynamicList(['David', 'Jack']);
   const listIndexes = list.map((item, index) => index);
 
   const Row = (index: number, item: any) => (
     <Auth.ReadRole>
-      <div key={getKey(index)} style={{ marginBottom: 16 }}>
+      <div key={getKey(index)}>
         <input
-          style={{ width: 300 }}
           placeholder="Please enter name"
           onChange={(e) => replace(index, e.target.value)}
           value={item}
         />
         <Auth.WriteRole>
           {list.length > 1 && (
-            <div
-              style={{ marginLeft: 8 }}
-              onClick={() => {remove(index)}}>(-)
-            </div>
+            <div onClick={() => { remove(index) }}>(-)</div>
           )}
-          <div
-            style={{ marginLeft: 8 }}
-            onClick={() => {
-              insert(index + 1, '');
-            }}>(+)</div>
+          <div onClick={() => { insert(index + 1, '') }}>(+)</div>
         </Auth.WriteRole>
       </div>
     </Auth.ReadRole>
@@ -38,10 +30,10 @@ export function Form(){
     <form data-ui='form'>
       {list.map((ele, index) => Row(index, ele))}
 
-      <section style={{ marginBottom: 16 }}>
+      <section>
         <Button
           onClick={() => batchRemove(listIndexes.filter((index) => index % 2 === 0))}
-        label="(-)" />
+          label="(-)" />
         <Button label="(-)"
           onClick={() => batchRemove(listIndexes.filter((index) => index % 2 !== 0))} />
       </section>
