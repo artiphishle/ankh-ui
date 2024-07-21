127343927a6cc346fcd82c50b2b3a1dc2c59de09
artiphishle <fabio.gartenmann@protonmail.com>
2024-07-22 01:52:54 +0200

    Add lucide-react

---
+++ lib/uis/nav/Nav.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Nav } from './Nav';
+
+const meta: Meta<typeof Nav> = {
+  component: Nav,
+};
+export default meta;
+
+type Story = StoryObj<typeof Nav>;
+
+export const NavTextOnly: Story = {
+  args: {
+    items: [
+      { name: "home" },
+      { name: "company" }
+    ]
+  },
+};
+
+export const NavWithIcons: Story = {
+  args: {
+    items: [
+      { name: "home", icon: "House" },
+      { name: "company", icon: "Fabric" }
+    ]
+  },
+};
