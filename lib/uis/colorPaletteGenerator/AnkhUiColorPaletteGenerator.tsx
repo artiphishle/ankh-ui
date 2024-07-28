159ab474d988c4c9b2939f3ee75e2866e8a0e13d
artiphishle <fabio.gartenmann@protonmail.com>
2024-07-28 16:19:35 +0200

    use client

--- lib/uis/colorPaletteGenerator/AnkhUiColorPaletteGenerator.tsx
+++ lib/uis/colorPaletteGenerator/AnkhUiColorPaletteGenerator.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useEffect, useState } from 'react';
 import { useColorPalette, useColorParser } from 'ankh-hooks';
 import { AnkhUiCircles, IAnkhUiCircles } from '@/uis/shapes/circles/AnkhUiCircles';
