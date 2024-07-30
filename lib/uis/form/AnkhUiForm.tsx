9000ba226ff6b5fc245ace8d9c97c46bbcc43a6c
artiphishle <fabio.gartenmann@protonmail.com>
2024-07-30 07:14:10 +0200

    Update form

--- lib/uis/form/AnkhUiForm.tsx
+++ lib/uis/form/AnkhUiForm.tsx
@@ -1,46 +1,13 @@
 'use client';
 import { useDynamicList } from 'ahooks';
-import { AnkhUiButton } from '@/uis/button/AnkhUiButton';
-import './form.css';
 import { Auth } from '@/auth/Auth';
-import { PropsWithChildren } from 'react';
+import './form.css';
+import { AnkhUiButton } from '../button/AnkhUiButton';
 
 export function AnkhUiForm({ items }: IAnkhUiForm) {
-  const { list, remove, batchRemove, getKey, insert, replace } = useDynamicList(items.map(({ value = '' }) => value));
-  const listIndexes = list.map((_, index) => index);
-
-  const Row = (index: number, item: IAnkhUiFormItem) => (
-    <Auth.ReadRole key={getKey(index)}>
-      <input
-        placeholder="Please enter name"
-        onChange={(e) => replace(index, e.target.value)}
-        title={item.title}
-        value={item.value}
-      />
-      <Auth.WriteRole>
-        {list.length > 1 && (
-          <div
-            onClick={() => {
-              remove(index);
-            }}
-          >
-            (-)
-          </div>
-        )}
-        <div
-          onClick={() => {
-            insert(index + 1, '');
-          }}
-        >
-          (+)
-        </div>
-      </Auth.WriteRole>
-    </Auth.ReadRole>
-  );
-
   return (
     <form data-ui="form">
-      <div>{JSON.stringify([list])}</div>
+      {items.map(({ placeholder = '', type = EAnkhUiFormInputType.Text }, index) => <input type={type} placeholder={placeholder} key={`form-field-${index}`} />)}
     </form>
   );
 }
@@ -54,7 +21,7 @@ enum EAnkhUiFormInputType {
 interface IAnkhUiFormItem {
   title: string;
   placeholder?: string;
-  type?: string;
+  type?: EAnkhUiFormInputType;
   value?: string;
 }
 interface IAnkhUiForm {
