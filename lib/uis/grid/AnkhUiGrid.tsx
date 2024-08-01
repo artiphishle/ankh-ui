696b978668816e9516cbdbf8e6dc6460778ba36a
artiphishle <fabio.gartenmann@protonmail.com>
2024-08-01 07:43:23 +0200

    Update grid columns in CMS

--- lib/uis/grid/AnkhUiGrid.tsx
+++ lib/uis/grid/AnkhUiGrid.tsx
@@ -1,14 +1,26 @@
-import { type PropsWithChildren, type ReactNode } from 'react';
+import { useState, type PropsWithChildren, type ReactNode } from 'react';
 import { Auth } from '@/auth/Auth';
 import './grid.css';
+import { AnkhUiButton } from '../button/AnkhUiButton';
+
+/*
+Think about Context and how to ingest to UI's FIRST!!
+1. Add 'id' for every UI in config
+2. hook to update props 'p' param in config 
+*/
 
 export function GridCell({ children }: IAnkhUiGridCell) {
   return <div data-ui="grid-cell">{children}</div>;
 }
 
-export function AnkhUiGrid({ children, columns = 1 }: IAnkhUiGrid) {
+export function AnkhUiGrid({ children, columns: initialColumns = 1 }: IAnkhUiGrid) {
+  const [columns, setColumns] = useState(initialColumns);
+
   return (
     <Auth.ReadRole>
+      <Auth.WriteRole>
+        <span><AnkhUiButton onClick={() => setColumns((prev) => prev - 1)} icon='minus' label='' />{columns} Cols<AnkhUiButton onClick={() => setColumns((prev) => prev + 1)} icon='plus' label='' /></span>
+      </Auth.WriteRole>
       <div data-ui="grid" style={{ gridTemplateColumns: `repeat(${columns}, 1fr)` }}>{children}</div>
     </Auth.ReadRole>
   );
@@ -18,7 +30,6 @@ interface IAnkhUiGridCell extends PropsWithChildren {
   id: string;
 }
 interface IAnkhUiGrid extends PropsWithChildren {
-  children: ReactNode[];
   columns?: number;
   styles?: Array<[string, string, string]>; // TStyle[]
 }
