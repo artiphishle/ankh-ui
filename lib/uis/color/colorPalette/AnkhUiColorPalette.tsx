ec0a09b57e9ed4801db4a66f35dde4a6e4023471
artiphishle <fabio.gartenmann@protonmail.com>
2024-09-10 02:43:54 +0200

    Display color information on circle click

--- lib/uis/color/colorPalette/AnkhUiColorPalette.tsx
+++ lib/uis/color/colorPalette/AnkhUiColorPalette.tsx
@@ -12,16 +12,58 @@ export function AnkhUiColorPalette({ name, colors }: IAnkhUiColorPalette) {
   const { getUsedColorTone } = useColorPalette();
   const [selectedTone, setSelectedTone] = useState<EAnkhColorTone>(getUsedColorTone(colors));
   const [isEditMode, setIsEditMode] = useState(false);
+  const [currentColorIndex, setCurrentColorIndex] = useState<number | null>(null);
   const toneOptions = Object.keys(EAnkhColorTone).map((toneName: string) => ({ name: toneName, value: toneName }));
   const circles: IAnkhUiCircle[] = colors.map(({ h, s, l }, colorIndex) => ({ _ui: { id: `color-${colorIndex}` }, style: { backgroundColor: `hsl(${h},${s}%,${l}%)` }, size: EAnkhUiSize.Sm }));
 
   const $e = {
     change: {
       tone: (event: ChangeEvent<HTMLElement>) => setSelectedTone(EAnkhColorTone[(event.target as HTMLSelectElement).value as keyof typeof EAnkhColorTone])
+    },
+    toggle: {
+      circle: (circleIndex: number) => isEditMode && setCurrentColorIndex(circleIndex)
     }
   };
 
-  const formItems: IAnkhUiFormItem[] = [
+  const formItemsEdit: IAnkhUiFormItem[] = [
+    {
+      title: "Name",
+      type: EAnkhUiFormInputType.Text,
+      value: name
+    },
+    {
+      title: "Tone",
+      type: EAnkhUiFormInputType.Text,
+      value: selectedTone,
+      // disabled: true
+    },
+    {
+      title: "Hue",
+      type: EAnkhUiFormInputType.Range,
+      value: colors[currentColorIndex as number]?.h || 0,
+      label: "H",
+      min: 0,
+      max: 360
+    },
+    {
+      title: "Saturation",
+      type: EAnkhUiFormInputType.Range,
+      value: colors[currentColorIndex as number]?.s || 0,
+      label: "S",
+      min: 0,
+      max: 100
+    },
+    {
+      title: "Lightness",
+      type: EAnkhUiFormInputType.Range,
+      value: colors[currentColorIndex as number]?.l || 0,
+      label: "L",
+      min: 0,
+      max: 100,
+    }
+  ];
+
+  const formItemsCreate: IAnkhUiFormItem[] = [
     // { title: "Hue", type: EAnkhUiFormInputType.Range, min: 0, max: 360, value: 240 },
     {
       title: "Tone",
@@ -37,15 +79,15 @@ export function AnkhUiColorPalette({ name, colors }: IAnkhUiColorPalette) {
       <div className="flex items-start bg-[#eee] p-[1rem] mb-[.4rem]" data-ui='color-palette'>
         <div className="flex-1">
           <h4>{name}</h4>
-          <AnkhUiCircles circles={circles} />
+          <AnkhUiCircles circles={circles} onCircleClick={$e.toggle.circle} />
         </div>
         <Auth.WriteRole>
           {isEditMode
             ?
             <>
-              <AnkhUiForm items={formItems} />
+              <AnkhUiForm items={formItemsEdit} />
               <AnkhUiButton label="OK" onClick={() => setIsEditMode(false)} />
-              <AnkhUiButton label="Cancel" onClick={() => setIsEditMode(false)} />
+              <AnkhUiButton label="Cancel" onClick={() => { setCurrentColorIndex(null); setIsEditMode(false) }} />
             </>
             :
             <AnkhUiButton icon="pencil" label='' onClick={() => setIsEditMode(true)} />
@@ -57,7 +99,7 @@ export function AnkhUiColorPalette({ name, colors }: IAnkhUiColorPalette) {
 }
 
 export interface IAnkhUiColorPalette {
-  name: string;
-  colors: IAnkhColorHsl[];
-  active?: boolean;
+  readonly name: string;
+  readonly colors: IAnkhColorHsl[];
+  readonly active?: boolean;
 }
--- lib/uis/shapes/circles/AnkhUiCircles.tsx
+++ lib/uis/shapes/circles/AnkhUiCircles.tsx
@@ -5,13 +5,13 @@ import {
 } from '@/uis/shapes/circle/AnkhUiCircle';
 import "./circles.css";
 
-export function AnkhUiCircles({ circles, title }: IAnkhUiCircles) {
+export function AnkhUiCircles({ circles, title, onCircleClick = () => { } }: IAnkhUiCircles) {
   return (
     <section data-ui="circles">
       {title && <AnkhUiHeading _ui={{ id: 'from-circles-423535' }} text={title} level="h3" />}
       <div className="circles">
-        {circles.map(({ style, size }, index) => (
-          <AnkhUiCircle _ui={{ id: 'from-circles-34545' }} key={index} style={style} size={size} />
+        {circles.map(({ style, size, active = false }, index) => (
+          <AnkhUiCircle _ui={{ id: 'from-circles-34545' }} key={index} style={style} size={size} active={active} onClick={() => onCircleClick(index)} />
         ))}
       </div>
     </section>
@@ -19,6 +19,7 @@ export function AnkhUiCircles({ circles, title }: IAnkhUiCircles) {
 }
 
 export interface IAnkhUiCircles {
-  circles: IAnkhUiCircle[];
-  title?: string;
+  readonly circles: IAnkhUiCircle[];
+  readonly title?: string;
+  readonly onCircleClick?: (circleIndex: number) => void;
 }
